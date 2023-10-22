5621f2f455af3078e714391524dd58e259e453a7
Joseph Kemper <JosephKemper@gmail.com>
2023-10-22 16:51:40 -0600

    Restoring project to where I finished

---
+++ app/README.md
@@ -0,0 +1,43 @@
+# Overview
+
+{Provide a description of the software that you wrote and how it integrates with a Cloud Database. Describe how to use your program.}
+In this project, I plan to build a simple app that will demonstrate my ability to lean new skills and technologies. I specifically plan to build a simple financial management app that will use Google Firebase, Jetpack Compose, and Kotlin. The app will let you track income and expenses, and create budgets.    
+
+My goals for creating this app is simply to challenge myself and prove that I can become a mobile developer. 
+
+
+[Software Demo Video -- Coming 28 October 2023](http://youtube.link.goes.here)
+
+# Cloud Database
+
+{Describe the cloud database you are using.}
+
+{Describe the structure of the database that you created.}
+
+# Development Environment
+
+{Describe the tools that you used to develop the software}
+
+{Describe the programming language that you used and any libraries.}
+
+# Useful Websites
+
+{Make a list of websites that you found helpful in this project}
+
+- [Build an Android app with Firebase and Jetpack Compose](https://firebase.google.com/codelabs/build-android-app-with-firebase-compose?hl=en#0)
+- [Jetpack Compose Intro](https://developer.android.com/jetpack/compose)
+- [Building an Android App a 9 part Series](https://firebase.blog/posts/2022/04/building-an-app-android-jetpack-compose-firebase)
+- [Android Studio Download](https://developer.android.com/studio)
+- [Android Emulator](https://developer.android.com/studio/run/emulator#install)
+- [Github Code for Firebase Task Manager app](https://github.com/FirebaseExtended/make-it-so-android.git)
+- [Firebase Setup Page](https://firebase.google.com/)
+- [Firebase Console Page](https://console.firebase.google.com/)
+- [Web Site Name](http://url.link.goes.here)
+
+# Future Work
+
+{Make a list of things that you need to fix, improve, and add in the future.}
+
+- Item 1
+- Item 2
+- Item 3
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,16 +1,22 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp")
+    id("com.google.gms.google-services")
+    id("com.google.firebase.crashlytics")
+    id("com.google.firebase.firebase-perf")
+    id("dagger.hilt.android.plugin")
+    id("com.ncorti.ktfmt.gradle") version "0.10.0"
 }
 
 android {
     namespace = "dev.techfirst.firebasefinanceapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "dev.techfirst.firebasefinanceapp"
         minSdk = 21
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -30,17 +36,18 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
+        buildConfig = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
+        kotlinCompilerExtensionVersion = "1.5.0"
     }
     packaging {
         resources {
@@ -50,20 +57,64 @@ android {
 }
 
 dependencies {
-
+    // Core libraries
     implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.10.0")
+
+    // Compose and UI libraries
+    implementation("androidx.compose:compose-bom:2023.10.00")
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.material:material")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material:material-icons-extended:1.5.3")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.material3:material3:1.1.2")
+
+    // Lifecycle and Activity libraries
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.2")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.0")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
-    testImplementation("junit:junit:4.13.2")
+
+    // Navigation and Preference libraries
+    implementation("androidx.navigation:navigation-compose:2.7.4")
+    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
+    implementation("androidx.preference:preference-ktx:1.2.1")
+
+    // Coroutines and Permissions libraries
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.6.4")
+    implementation("com.google.accompanist:accompanist-permissions:0.30.1")
+
+    // Dagger Hilt libraries
+    implementation("com.google.dagger:hilt-android:2.47")
+    ksp("com.google.dagger:hilt-compiler:2.47")
+
+    // Firebase libraries
+    implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
+    implementation("com.google.firebase:firebase-analytics-ktx")
+    implementation("com.google.firebase:firebase-crashlytics-ktx")
+    implementation("com.google.firebase:firebase-auth-ktx")
+    implementation("com.google.firebase:firebase-firestore-ktx")
+    implementation("com.google.firebase:firebase-perf-ktx")
+    implementation("com.google.firebase:firebase-config-ktx")
+    implementation("com.google.firebase:firebase-messaging-ktx")
+
+    // Test libraries
+    testImplementation("junit:junit:4.+")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+
+    // Dagger Hilt testing library
+    androidTestImplementation("com.google.dagger:hilt-android-testing:2.43.2")
+
+    // Truth library for fluent testing
+    androidTestImplementation("com.google.truth:truth:1.1.3")
+
+    // Dagger Hilt compiler for Android tests
+    kspAndroidTest("com.google.dagger:hilt-compiler:2.47")
+
+    // Debugging libraries
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
 }
