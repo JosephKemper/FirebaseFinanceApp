47754b36782501b7086deebbf39fb24d465c6884
Joseph Kemper <JosephKemper@gmail.com>
2023-10-28 17:54:18 -0600

    Added comments

--- app/src/main/java/dev/techfirst/firebasefinanceapp/MainActivity.kt
+++ app/src/main/java/dev/techfirst/firebasefinanceapp/MainActivity.kt
@@ -49,17 +49,17 @@ class MainActivity : ComponentActivity() {
 fun convertTransaction(rawTransactionAmount: String): BigDecimal {
     return try {
         BigDecimal(rawTransactionAmount) // string to BigDecimal
-    } catch (numberConversionException: NumberFormatException) {
+    } catch (invalidTransactionAmountException: NumberFormatException) {
         BigDecimal("0.00") // Replace an invalid entry with 0.00
     }
 }
 
 // Ensure the value entered by the user was entered as a positive number
 fun ensurePositive (transaction: BigDecimal): BigDecimal {
-    return if (transaction < BigDecimal.ZERO){
-        transaction.negate()
-    } else {
+    return if (transaction >= BigDecimal.ZERO){
         transaction
+    } else {
+        transaction.negate()
     }
 }
 
@@ -74,21 +74,23 @@ fun convertToDate (dateString: String): Date {
 
 }
 
-fun updateTotal(transactionAmount: BigDecimal, isDeposit: Boolean){
-    if (isDeposit) {
-        availableBalance += transactionAmount
-    } else {
+// Keep a running total of the balance of the account for display on the app
+fun updateBalance(transactionAmount: BigDecimal, isDeposit: Boolean){
+    if (isDeposit) { // Check if the transaction is a deposit
+        availableBalance += transactionAmount // add to balance
+    } else { // Otherwise subtract from the balance
         availableBalance -= transactionAmount
     }
 
 }
 
 // Get Transaction data ready to store in transactionList and adds it to the list
-fun processTransaction(rawTransactionAmount: String, rawDate: String, description: String?, isDeposit: Boolean){
-    val decimalTransaction = (rawTransactionAmount)
-    val transactionAmount = ensurePositive(decimalTransaction)
-    val date = convertToDate (rawDate)
-    updateTotal(transactionAmount,isDeposit)
+fun processTransaction(transactionAmountAsString: String, rawDate: String, description: String?, isDeposit: Boolean){
+    val transactionAmount = ensurePositive(BigDecimal(transactionAmountAsString)) // Convert transaction string to number
+    val date = convertToDate (rawDate) // Format the date string
+    updateBalance(transactionAmount,isDeposit) // update the current balance
+    // Create instance of the Transaction data class and assign it to the transaction variable
     val transaction = TransactionData (transactionAmount, date, description, isDeposit)
+    // add transaction to list
     transactionList.add(transaction)
 }
